ebf41ed615a0d7889e93812082900d20b28c373a
Othman Rhajbal <othman.rhajbal00@gmail.com>
2024-07-28 21:28:07 +0000

    ES6 promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,12 @@
-
 export default function getFullResponseFromAPI(success) {
-  return new Promise((res, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      res({
+      resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
