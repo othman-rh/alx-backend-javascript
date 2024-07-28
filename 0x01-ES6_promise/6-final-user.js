de79e9e9159e5fbfbd19a3d94dca4e1d34b25a07
Othman Rhajbal <othman.rhajbal00@gmail.com>
2024-07-28 21:25:28 +0000

    ES6 promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,13 @@
-import signUpUser from './4-all-reject';
-import uploadPhoto from './5-all-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const res = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    });
-  }
-  return res;
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
